e315a7a59c3361f37b1f30944c8093fa719775c0
formation <formation@FORMATI-KL6GVKB.formation.at>
2017-06-01 17:08:04 +0200

    version

--- src/main/java/reservation/controller/Reservationcontroller.java
+++ src/main/java/reservation/controller/Reservationcontroller.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import reservation.DTO.reservationDTO;
-import reservation.entity.Reservation;
 
 /**
  *
